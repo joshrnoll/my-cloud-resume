bae5a7dcbe28c9e25e2f6556596bd778ddc91fc8
Joshua <noll.joshua@outlook.com>
2025-07-14 15:48:00 -0400

    initial commit

---
+++ make.py
@@ -0,0 +1,13 @@
+from jinja2 import Environment, FileSystemLoader
+import yaml
+
+with open('./resume.yaml', 'r') as f:
+    values = yaml.full_load(f)
+
+env = Environment(loader = FileSystemLoader('./templates'))
+
+template = env.get_template('resume.html.jinja')
+output = template.render(values)
+
+with open('./resume.html', 'w') as f:
+    f.write(output)
